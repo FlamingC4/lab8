4a70bed001a49cd39a77267fc2358cae26f86cba
Aaron Wong <flamingc4@gmail.com>
2018-03-01 02:36:22 -0800

    edits

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -3,6 +3,7 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
   initializePage();
+
 })
 
 /*
@@ -17,10 +18,44 @@ function initializePage() {
 
 // init jQuery gestures  
 function initGestures() {
+
 	// add gestures listener here
-}
+	$(function(){
+		$(".judge-img").bind("taphold", tapholdHandler);
+
+		function tapholdHandler(event){
+			//get the id of the event source
+			var targetIDPrefix = event.target.id;
+			console.log("got prefix:" + targetIDPrefix);
+			//show bio
+			$("#" + targetIDPrefix + "-bio").show();
+		}
+	});
+} 
 
 // init RSVP form submit listener
 function initRSVPForm() {
-  // add your code here
+	$('#rsvpForm').submit(function(e){
+		e.preventDefault();
+		console.log("submitting form...");
+		var rsvpEmail = $('#rsvpEmail').val();
+		$.post('addRSVP', { rsvpEmail: rsvpEmail }, postCallback);
+	});
+	
+  function postCallback(res){
+  	alert("RSVP form successfully submitted!");
+  	$('#rsvpEmail').val('');
+  }
 }
+/*function initGestures() {
+	$(function(){
+		$(".judge-img").bind("taphold", tapholdHandler);
+
+		function tapholdHandler(event){
+			var targetIDPrefix = event.target.id;
+			console.log("got prefix:" + targetIDPrefix);
+			$("#" + targetIDPrefix + "-bio").show();
+
+		}
+	});
+}*/
